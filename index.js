87b19aedd71d42104e5ff952844946486bd32a90
FifkaFo <fireorbdhr@gmail.com>
2021-10-12 16:31:58 +0300

    added randomize stickers

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const bot = new TelegramApi(token, {polling: true})
 
 const {gameOptions, againOptions} = require('./options')
 
+const stickersHello = [
+    "https://tlgrm.ru/_/stickers/076/1ba/0761ba7d-566f-4e46-ab05-367a1747f16b/1.webp", // Ями-тян ч.2
+    "https://tlgrm.ru/_/stickers/379/9b9/3799b968-4bcd-3486-aac2-000bbbd13248/1.webp", // путин-привет
+    "https://tlgrm.ru/_/stickers/ed2/06b/ed206b2c-6599-44f3-ae2a-94010e132505/1.webp", // Эмми
+    "https://tlgrm.ru/_/stickers/944/696/9446968f-a27b-4103-a9ee-6e71c093319f/1.webp", // pink haired boy
+    "https://tlgrm.ru/_/stickers/88e/586/88e586f0-4299-313f-bedb-ef45c7710422/1.webp" // Авокадик
+]
+const stickersBadMood = [
+    "https://tlgrm.ru/_/stickers/2e4/c45/2e4c45a5-d93f-42d9-a0d6-027449c580c0/192/27.webp", // Мей
+    "https://tlgrm.ru/_/stickers/88e/586/88e586f0-4299-313f-bedb-ef45c7710422/6.webp", // Авокадик
+    "https://tlgrm.ru/_/stickers/742/cb9/742cb955-33c0-4e5e-9497-9c798aae0058/2.webp" // Мини Нэко
+]
+const stickersGoodMood = [
+    "https://tlgrm.ru/_/stickers/742/cb9/742cb955-33c0-4e5e-9497-9c798aae0058/9.webp", // Мини Нэко
+    "https://tlgrm.ru/_/stickers/742/cb9/742cb955-33c0-4e5e-9497-9c798aae0058/1.webp", // Мини Нэко
+    "https://tlgrm.ru/_/stickers/076/1ba/0761ba7d-566f-4e46-ab05-367a1747f16b/3.webp", // Ями-тян ч.2
+    "https://tlgrm.ru/_/stickers/076/1ba/0761ba7d-566f-4e46-ab05-367a1747f16b/192/23.webp" // Ями-тян ч.2
+]
 const chats = {}
 
 const startGame = async (chatId) => {
         await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен её угадать!`)
-        const  randomNumber = Math.
-        floor(Math.random() * 10)
+        const  randomNumber = Math.floor(Math.random() * 10)
         chats[chatId] = randomNumber
         await bot.sendMessage(chatId, 'Отгадывай', gameOptions)
 }
@@ -33,18 +50,19 @@ const start = () => {
 
         if(text.toLowerCase() === '/start') {
             await bot.sendMessage(chatId, `Приветствую тебя ${msg.from.first_name}!`)
-            return  bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/c62/4a8/c624a88d-1fe3-403a-b41a-3cdb9bf05b8a/1.webp')
+            return  bot.sendSticker(chatId,  stickersHello[Math.floor(Math.random() * stickersHello.length)])
         }
 
         if(text.toLowerCase() === '/info') {
             return  bot.sendMessage(chatId, `Для общения со мной доступны такие команды:\n
         /start - начать общение со мной\n
-        /info - список всех команд для общения со мной`)
+        /info - список всех команд для общения со мной\n
+        /game - Сыграть в игру "Угадай цифру"`)
         }
         if(text === '/game') {
             return startGame(chatId)
         }
-        return bot.sendMessage(chatId,'Я тебя не понял')
+        return bot.sendMessage(chatId,'Я тебя не понял, /info - список команд')
     })
 
     bot.on('callback_query', msg => {
@@ -53,14 +71,14 @@ const start = () => {
 
         if(data === '/again') {
             startGame(chatId)
-        }
-        if(data+"" === chats[chatId]+"") {
-            return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions)
+        } else if(data+"" === chats[chatId]+"") {
+            bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`)
+            return bot.sendSticker(chatId, stickersGoodMood[Math.floor(Math.random() * stickersGoodMood.length)], againOptions)
         } else {
-            console.log(data + " | " + chats[chatId])
-            return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againOptions)
+            //console.log(data + " | " + chats[chatId])
+            bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`)
+            return bot.sendSticker(chatId, stickersBadMood[Math.floor(Math.random() * stickersBadMood.length)], againOptions)
         }
     })
 }
-
 start()
